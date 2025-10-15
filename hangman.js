d5e65badd8746801f2f5fee2c300a0bf01d8cd0c
Hajrah Hameed <hajrahhameed56@gmail.com>
2025-10-15 10:45:33 -0400

    Pick random word

--- hangman.js
+++ hangman.js
@@ -1,4 +1,4 @@
-var POSSIBLE_WORDS: ["obdurate", "verisimilitude", 
+var POSSIBLE_WORDS= ["obdurate", "verisimilitude", 
     "defenestrate", "obsequious", "dissonant", "toady","idempotent"];
 
 var randomIndex = parseInt(Math.random() * POSSIBLE_WORDS.length);
