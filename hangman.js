d1034416cbfe6053bdb70d21f3197152b7170375
Hajrah Hameed <hajrahhameed56@gmail.com>
2025-10-15 10:42:55 -0400

    Add basic styling

---
+++ hangman.js
@@ -0,0 +1,5 @@
+var POSSIBLE_WORDS: ["obdurate", "verisimilitude", 
+    "defenestrate", "obsequious", "dissonant", "toady","idempotent"];
+
+var randomIndex = parseInt(Math.random() * POSSIBLE_WORDS.length);
+var word = POSSIBLE_WORDS[randomIndex];
