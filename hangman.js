97878b684ba2dc7fceb3ba08adcb9d1667972fbb
Hajrah Hameed <hajrahhameed56@gmail.com>
2025-10-20 10:05:26 -0400

    First version of new game logic

--- hangman.js
+++ hangman.js
@@ -1,5 +1,16 @@
 var POSSIBLE_WORDS= ["obdurate", "verisimilitude", 
     "defenestrate", "obsequious", "dissonant", "toady","idempotent"];
+var word="";
 
-var randomIndex = parseInt(Math.random() * POSSIBLE_WORDS.length);
-var word = POSSIBLE_WORDS[randomIndex];
+function newGame(){
+    var randomIndex = parseInt(Math.random() * POSSIBLE_WORDS.length);
+    word = POSSIBLE_WORDS(randomIndex);
+
+    var clueString = "";
+    for(var i = 0;i<word.length;i++)
+    {
+        clueString+= "_";
+    }    
+    var clue = document.getElementById("clue");
+    clue.innerHTML = clueString;
+}
