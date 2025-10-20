46b27df58b25312fee2acf1be1a568d469227902
Hajrah Hameed <hajrahhameed56@gmail.com>
2025-10-20 10:34:22 -0400

    added guess letter functionality

--- hangman.js
+++ hangman.js
@@ -1,16 +1,32 @@
 var POSSIBLE_WORDS= ["obdurate", "verisimilitude", 
     "defenestrate", "obsequious", "dissonant", "toady","idempotent"];
-var word="";
-
+var word = "";
+var guesses = "";
 function newGame(){
     var randomIndex = parseInt(Math.random() * POSSIBLE_WORDS.length);
     word = POSSIBLE_WORDS(randomIndex);
-
+    guesses = ""
+    updatePage();
+}
+function guessLetter() {
+    var input = document.getElementById("guess");
+    var letter = input.value;
+    guess+=letter;
+    updatePage();
+}
+function updatePage() {
     var clueString = "";
-    for(var i = 0;i<word.length;i++)
-    {
-        clueString+= "_";
-    }    
+    for(var i = 0;i < word.length;i++) {
+        var currentLetter = word.charAt(i);
+        if (guesses.indexOf(currentLettr) >= 0 ) {//You guessed it
+            clueString += currentLetter + " ";
+        }
+        else
+            clueString += "_ ";
+    }
     var clue = document.getElementById("clue");
     clue.innerHTML = clueString;
+
+    var guessArea = document.getElementById("guesses");
+    guessArea.innerHTML = "Guessed Letters: " + guesses;
 }
