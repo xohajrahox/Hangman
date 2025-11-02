85076405aa36c82aa183d5cafc8ac505260c7544
Hajrah Hameed <hajrahhameed56@gmail.com>
2025-11-02 12:55:30 -0500

    Added a gameOver variable to know when the game ends

--- hangman.js
+++ hangman.js
@@ -4,10 +4,12 @@ var MAX_GUESSES = 6;
 var word = "";
 var guesses = ""; 
 var guess_count = MAX_GUESSES;
+var gameOver = false;
+
 
 function newGame() {
     var randomIndex = parseInt(Math.random() * POSSIBLE_WORDS.length);
-    word = POSSIBLE_WORDS(randomIndex);
+    word = POSSIBLE_WORDS[randomIndex];
     guesses = ""
     guess_count = MAX_GUESSES;
     updatePage();
@@ -15,14 +17,15 @@ function newGame() {
 function guessLetter() {
     var input = document.getElementById("guess");
     var letter = input.value;
-    if(word.indexOf(letter) < 0 ){
+    if (word.indexOf(letter) < 0 ) {
         guess_count--;
     }
     guesses += letter;
     updatePage();
+    input.value = "";
 }
 function updatePage() {
-    var clueString =  "";
+    var clueString = "";
     for (var i = 0; i < word.length; i++) {
         var currentLetter = word.charAt(i);
         if (guesses.indexOf(currentLetter) >= 0 ) {//You guessed it
@@ -41,5 +44,5 @@ function updatePage() {
 
     //update the image
     var image = document.getElementById("hangmanImage");
-    image.src =  "images/hangman" + guess_count + ".gif"; 
+    image.src =  "images/hangman" + guess_count + ".gif";
 }
